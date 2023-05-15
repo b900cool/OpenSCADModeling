a80bcbc29f55083f60cf6c6945602616a0e974f7
Bobby Belter <b900cool@gmail.com>
2023-05-14 17:15:15 -0700

    More robust use of module

--- Powershell/DownloadLibraries.ps1
+++ Powershell/DownloadLibraries.ps1
@@ -1,5 +1,7 @@
-Import-Module -Name '.\GithubLibraries.psd1'
+$modulePath = Join-Path$PSScriptRoot '\GithubLibraries.psd1'
+Import-Module -Name $modulePath
 
+Save-GitHubRepository -Owner rcolyer -Project threads-scad
 Save-GitHubRepository -Owner Obijuan -Project obiscad
 Save-GitHubRepository -Owner revarbat -Project BOSL
 Save-GitHubReleaseTar -Owner boltsparts -Project BOLTS_archive -ArtifactName boltsos_0.4.1.tar.gz
